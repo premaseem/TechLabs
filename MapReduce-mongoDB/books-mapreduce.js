2b291df0697b9b5c1d0f54d499bea9c0629f53e1
Aseem Jain <premaseem.webmaster@gmail.com>
2020-08-07 19:15:10 -0500

    Map Reduce explained with books data set

---
+++ MapReduce-mongoDB/books-mapreduce.js
@@ -0,0 +1,355 @@
+I have explained in youtube tutorial video: 
+https://www.youtube.com/watch?v=tzEHT2YfM30
+
+
+
+
+// insert data set 
+
+db.books.insert(
+{
+	"_id" : ObjectId("5f2de2ad29d38cb899d259fa"),
+	"title" : "MongoDB and Python: Patterns and processes for the popular document-oriented database",
+	"published" : "2011-09-30",
+	"authors" : [
+		{
+			"firstName" : " Niall",
+			"lastName" : "O'Higgins"
+		}
+	],
+	"categories" : [
+		"Databases",
+		"NoSQL",
+		"Programming"
+	],
+	"publisher" : {
+		"name" : "O'Reilly"
+	},
+	"price" : 18.06
+}
+{
+	"_id" : ObjectId("5f2de2b429d38cb899d259fb"),
+	"title" : " Node.js in Action",
+	"published" : "2013-11-28",
+	"authors" : [
+		{
+			"firstName" : " Mike",
+			"lastName" : "Cantelon"
+		}
+	],
+	"categories" : [
+		"Databases",
+		"NoSQL",
+		"Programming",
+		"Web"
+	],
+	"publisher" : {
+		"name" : "Manning"
+	},
+	"price" : 26.09
+}
+{
+	"_id" : ObjectId("5f2de4c529d38cb899d259fc"),
+	"title" : "MongoDB: The Definitive Guide",
+	"published" : "2013-05-23",
+	"authors" : [
+		{
+			"firstName" : "Kristina",
+			"lastName" : "Chodorow"
+		}
+	],
+	"categories" : [
+		"Databases",
+		"NoSQL",
+		"Programming"
+	],
+	"publisher" : {
+		"name" : "O'Reilly"
+	},
+	"price" : 32.99
+}
+{
+	"_id" : ObjectId("5f2de4d129d38cb899d259fd"),
+	"title" : "MongoDB Applied Design Patterns",
+	"published" : "2013-03-19",
+	"authors" : [
+		{
+			"firstName" : "Rick",
+			"lastName" : "Copeland"
+		}
+	],
+	"categories" : [
+		"Databases",
+		"NoSQL",
+		"Patterns",
+		"Programming"
+	],
+	"publisher" : {
+		"name" : "O'Reilly"
+	},
+	"price" : 32.99
+}
+{
+	"_id" : ObjectId("5f2de4d729d38cb899d259fe"),
+	"title" : "MongoDB in Action",
+	"published" : "2011-12-16",
+	"authors" : [
+		{
+			"firstName" : "Kyle",
+			"lastName" : "Banker"
+		}
+	],
+	"categories" : [
+		"Databases",
+		"NoSQL",
+		"Programming"
+	],
+	"publisher" : {
+		"name" : "Manning"
+	},
+	"price" : 30.83
+}
+{
+	"_id" : ObjectId("5f2de4dd29d38cb899d259ff"),
+	"title" : "NoSQL Distilled: A Brief Guide to the Emerging World of Polyglot Persistence",
+	"published" : "2012-08-18",
+	"authors" : [
+		{
+			"firstName" : "Pramod J.",
+			"lastName" : "Sadalage"
+		},
+		{
+			"firstName" : "Martin",
+			"lastName" : "Fowler"
+		}
+	],
+	"categories" : [
+		"Databases",
+		"NoSQL"
+	],
+	"publisher" : {
+		"name" : "Addison Wesley"
+	},
+	"price" : 26.36
+}
+{
+	"_id" : ObjectId("5f2de4e329d38cb899d25a00"),
+	"title" : "Scaling MongoDB",
+	"published" : "2011-03-07",
+	"authors" : [
+		{
+			"firstName" : "Kristina",
+			"lastName" : "Chodorow"
+		}
+	],
+	"categories" : [
+		"Databases",
+		"NoSQL"
+	],
+	"publisher" : {
+		"name" : "O'Reilly"
+	},
+	"price" : 25.3
+}
+{
+	"_id" : ObjectId("5f2de4e929d38cb899d25a01"),
+	"title" : "50 Tips and Tricks for MongoDB Developers",
+	"published" : "2011-05-06",
+	"authors" : [
+		{
+			"firstName" : "Kristina",
+			"lastName" : "Chodorow"
+		}
+	],
+	"categories" : [
+		"Databases",
+		"NoSQL",
+		"Programming"
+	],
+	"publisher" : {
+		"name" : "O'Reilly"
+	},
+	"price" : 25.08
+}
+{
+	"_id" : ObjectId("5f2de50229d38cb899d25a02"),
+	"title" : "MongoDB in Action, 2nd Edition",
+	"published" : "2014-12-01",
+	"authors" : [
+		{
+			"firstName" : "Kyle",
+			"lastName" : "Banker"
+		},
+		{
+			"firstName" : "Peter",
+			"lastName" : "Bakkum"
+		},
+		{
+			"firstName" : "Tim",
+			"lastName" : "Hawkins"
+		}
+	],
+	"categories" : [
+		"Databases",
+		"NoSQL",
+		"Programming"
+	],
+	"publisher" : {
+		"name" : "Manning"
+	},
+	"price" : 26.66
+}
+{
+	"_id" : ObjectId("5f2de51429d38cb899d25a03"),
+	"title" : "NoSQL Distilled: A Brief Guide to the Emerging World of Polyglot Persistence",
+	"published" : "2012-08-18",
+	"authors" : [
+		{
+			"firstName" : "Pramod J.",
+			"lastName" : "Sadalage"
+		},
+		{
+			"firstName" : "Martin",
+			"lastName" : "Fowler"
+		}
+	],
+	"categories" : [
+		"Databases",
+		"NoSQL"
+	],
+	"publisher" : {
+		"name" : "Addison Wesley"
+	},
+	"price" : 26.36
+}
+{
+	"_id" : ObjectId("5f2de8c429d38cb899d25a04"),
+	"title" : " Node.js in Action",
+	"published" : "2013-11-28",
+	"authors" : [
+		{
+			"firstName" : " Mike",
+			"lastName" : "Cantelon"
+		}
+	],
+	"categories" : [
+		"Databases",
+		"NoSQL",
+		"Programming",
+		"Web"
+	],
+	"publisher" : {
+		"name" : "Manning"
+	},
+	"price" : 26.09
+}
+{
+	"_id" : ObjectId("5f2de8c929d38cb899d25a05"),
+	"title" : "MongoDB and Python: Patterns and processes for the popular document-oriented database",
+	"published" : "2011-09-30",
+	"authors" : [
+		{
+			"firstName" : " Niall",
+			"lastName" : "O'Higgins"
+		}
+	],
+	"categories" : [
+		"Databases",
+		"NoSQL",
+		"Programming"
+	],
+	"publisher" : {
+		"name" : "O'Reilly"
+	},
+	"price" : 18.06
+}
+);
+
+
+
+============================================================
+
+
+// Now run different Map Reduce functions to get concluding results 
+
+
+// Count books by author
+
+db.runCommand( {
+    mapReduce: "books",
+    map: function() {
+        for (var index = 0; index < this.authors.length; ++index) {
+            var author = this.authors[ index ];
+            emit( author.firstName + " " + author.lastName, 1 );
+        }
+    },
+    reduce: function(author, counters) {
+        count = 0;
+ 
+        for (var index = 0; index < counters.length; ++index) {
+            count += counters[index];
+        }
+ 
+        return count;
+    },
+    out: { inline: 1 }
+} )
+
+
+// Count average book price by publisher
+db.runCommand( {
+    mapReduce: "books",
+    scope: { currency: "US" },
+    map: function() {
+        emit( this.publisher, { count: 1, price: this.price } );
+    },
+    reduce: function(publisher, values) {
+        var value = { count: 0, price: 0 };
+ 
+        for (var index = 0; index < values.length; ++index) {
+            value.count += values[index].count;
+            value.price += values[index].price;
+        }
+ 
+        return value;
+    },
+    finalize: function(publisher, value) {
+        value.average = currency + ( value.price / value.count ).toFixed(2);
+        return value;
+    },
+    out: {
+        replace: "results"
+    }
+} )
+
+
+
+
+// Incrementally count average book price by publisher
+
+db.runCommand( {
+    mapReduce: "books",
+    scope: { currency: "US" },
+    map: function() {
+        emit( this.publisher, { count: 1, price: this.price } );
+    },
+    reduce: function(publisher, values) {
+        var value = { count: 0, price: 0 };
+ 
+        for (var index = 0; index < values.length; ++index) {
+            value.count += values[index].count;
+            value.price += values[index].price;
+        }
+ 
+        return value;
+    },
+    finalize: function(publisher, value) {
+        value.average = currency + ( value.price / value.count ).toFixed(2);
+        return value;
+    },
+    query: { "authors.lastName": { $in: [ "Cantelon", "O'Higgins" ] } },
+    limit: 2,
+    out: {
+        reduce: "results"
+    }
+} )
